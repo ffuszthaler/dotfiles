03776b54878051a44f2ba795dc38125f8a4c36aa
ffuszthaler <flo.fuszthaler@gmail.com>
2020-02-23 14:25:08 +0100

    changed emacs config

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -6,9 +6,9 @@
  '(custom-enabled-themes (quote (doom-moonlight)))
  '(custom-safe-themes
    (quote
-    ("a4b9eeeabde73db909e6b080baf29d629507b44276e17c0c411ed5431faf87dd" "92d8a13d08e16c4d2c027990f4d69f0ce0833c844dcaad3c8226ae278181d5f3" "5091eadbb87fa0a168a65f2c3e579d1a648d764f12ab9d3ab7bdefca709cd2a5" "5c9a906b076fe3e829d030a404066d7949e2c6c89fc4a9b7f48c054333519ee7" "a02836a5807a687c982d47728e54ff42a91bc9e6621f7fe7205b0225db677f07" "1897b97f63e91a792e8540c06402f29d5edcbfb0aafd64b1b14270663d6868ee" default)))
+    ("774aa2e67af37a26625f8b8c86f4557edb0bac5426ae061991a7a1a4b1c7e375" "a4b9eeeabde73db909e6b080baf29d629507b44276e17c0c411ed5431faf87dd" "92d8a13d08e16c4d2c027990f4d69f0ce0833c844dcaad3c8226ae278181d5f3" "5091eadbb87fa0a168a65f2c3e579d1a648d764f12ab9d3ab7bdefca709cd2a5" "5c9a906b076fe3e829d030a404066d7949e2c6c89fc4a9b7f48c054333519ee7" "a02836a5807a687c982d47728e54ff42a91bc9e6621f7fe7205b0225db677f07" "1897b97f63e91a792e8540c06402f29d5edcbfb0aafd64b1b14270663d6868ee" default)))
  '(lsp-auto-guess-root nil)
- '(lsp-prefer-flymake nil)
+ '(lsp-prefer-flymake nil t)
  '(package-selected-packages
    (quote
     (auto-package-update company-lsp company lsp-mode yasnippet-classic-snippets yasnippet-snippets yasnippet magit flycheck projectile neotree centaur-tabs nyan-mode doom-modeline doom-themes evil smex use-package))))
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -157,7 +157,7 @@
   :ensure t
   :config
   (setq auto-package-update-delete-old-versions t
-        auto-package-update-interval 4)
+        auto-package-update-interval 7)
   (auto-package-update-maybe))
 
 ;; Org-Mode
